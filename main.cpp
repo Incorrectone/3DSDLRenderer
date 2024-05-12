7a66d9da515e7be52275e6c6fa72941310206b46
Incorrectone <incorrectmail101@gmail.com>
2024-05-12 18:11:15 -0400

    Update Code with Comments for future proofing

--- main.cpp
+++ main.cpp
@@ -1,56 +1,52 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2024
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
+#include "custom/include/constants.h"
+#include "custom/include/putPixel.h"
 #include "external/SDL2/include/SDL.h"
 #include <stdio.h>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+int main(void){
+	SDL_Event event; // Basic Event Union
+	SDL_Window* window = NULL; // The Window we will be rendering to
 
-int main( int argc, char* args[] )
-{
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+	// Surface gives Blit Access to Pixels on CPU
+	// SDL_Renderer works on GPU 
+	SDL_Surface* screenSurface = NULL; // The Surface that fills the Window
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
+	// Starting up Video System of SDL
+	// https://www.libsdl.org/release/SDL-1.2.15/docs/html/guidebasicsinit.html
+	if(SDL_Init(SDL_INIT_VIDEO)<0){ // Call to function also Initiliasize it
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
-
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
-            
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-		}
+		return -1;
 	}
 
-	//Destroy window
-	SDL_DestroyWindow( window );
+	// The Actual Window
+	window = SDL_CreateWindow("3D Renderer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, constants::SCREEN_WIDTH, constants::SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	
+	// Check If window Pointer got created
+	if(window == NULL){
+		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		return -1;
+	}
 
-	//Quit SDL subsystems
-	SDL_Quit();
+	// Create the Surface to which we will draw
+	screenSurface = SDL_GetWindowSurface(window);
+
+	//Fill the surface white
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+	
+	putPixel(screenSurface, 0, 0, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
+
+	//Update the surface
+	SDL_UpdateWindowSurface(window);
+
+
+	// Game Loop
+	while(1){
+        if(SDL_PollEvent(&event) && event.type == SDL_QUIT){
+            break;
+		}
+    }
 
 	return 0;
 }
+
+
