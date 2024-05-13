7602942ff7c80d5687ec0eea03e003d08f2f9fcc
Incorrectone <incorrectmail101@gmail.com>
2024-05-13 13:17:14 -0400

    Made it visually appealing and made the function VOID

--- custom/putPixel.cpp
+++ custom/putPixel.cpp
@@ -2,19 +2,22 @@
 #include "include/constants.h"
 #include "../external/SDL2/include/SDL.h"
 
-int putPixel(SDL_Surface* surface, int canvasX, int canvasY, Uint32 color){
+// Basic Pixel Changing function
+void putPixel(SDL_Surface* surface, int canvasX, int canvasY, Uint32 color){
+
+	// If the coordinates are outside the screen, Does Nothing
 	if(canvasX > (constants::SCREEN_WIDTH / 2) || canvasX < -(constants::SCREEN_WIDTH / 2))
-		return -1;
+		return (void)1;
 	if(canvasY > (constants::SCREEN_HEIGHT / 2) || canvasY < -(constants::SCREEN_HEIGHT / 2))
-		return -1;
+		return (void)1;
 	
 	// Pg. 3
 	int screenX = (constants::SCREEN_WIDTH / 2) + canvasX;
-	int screenY = (constants::SCREEN_HEIGHT / 2) + canvasY;
+	int screenY = (constants::SCREEN_HEIGHT / 2) - canvasY;
 
 	Uint32 * const target_pixel = (Uint32 *) ((Uint8 *) surface->pixels
                                              + screenY * surface->pitch
                                              + screenX * surface->format->BytesPerPixel);
 	*target_pixel = color;
-	return 0;
+	return (void)0;
 }
