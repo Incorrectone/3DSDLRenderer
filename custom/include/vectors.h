bb38e02603d3ba8c134a5bbef19dc4cc77897db1
Incorrectone <incorrectmail101@gmail.com>
2024-05-23 00:31:13 +0530

    Made a vector struct with the help of a CPP book Moved vectormath functions in here as operators

--- custom/include/vectors.h
+++ custom/include/vectors.h
@@ -3,17 +3,125 @@
 #ifndef VECTORS_H
 #define VECTORS_H
 
-namespace VEC{
-    struct VECTOR3D{
-        double x;
-        double y;
-        double z;
-    };
+#include <iostream>
+#include <cmath>
 
-    struct VECTOR2D{
-        double x;
-        double y;
-    };
-} 
+template<typename T>
+struct Vector3D{
+    T x, y, z;
+
+    Vector3D(T a = 0, T b = 0, T c = 0) : x{a}, y{b}, z{c} {} // Constructor general
+
+    // Constructor for <int> type to <double> type and vice versa
+    template <typename D> 
+        Vector3D(const Vector3D<D> &other){ 
+            x = other.x; 
+            y = other.y; 
+            z = other.z; 
+        }
+
+    template<typename D>
+        inline Vector3D& operator+=(const Vector3D<D> a);
+
+    template<typename D>
+        inline Vector3D& operator-=(const Vector3D<D> a);
+
+    Vector3D& operator*=(const double a);
+
+    T modulus() const;
+
+    template<typename D>
+        T dot(Vector3D<D> a) const;
+
+    Vector3D& colorFit();
+
+};
+
+template<typename T>
+    std::ostream& operator<<(std::ostream& os, const Vector3D<T> &A){
+        return os << "<Vector3D[" << &A << "](" << A.x << ", " << A.y << ", " << A.z << ")>";
+    }
+
+template <typename T>
+template <typename D>
+    inline Vector3D<T> &Vector3D<T>::operator+=(const Vector3D<D> a){
+        this->x += a.x;
+        this->y += a.y;
+        this->z += a.z;
+        return *this;
+    }
+
+template <typename T>
+template <typename D>
+    inline Vector3D<T> &Vector3D<T>::operator-=(const Vector3D<D> a){
+        this->x -= a.x;
+        this->y -= a.y;
+        this->z -= a.z;
+        return *this;
+    }
+
+template <typename T>
+    inline Vector3D<T> &Vector3D<T>::operator*=(const double a){
+        this->x *= a;
+        this->y *= a;
+        this->z *= a;
+        return *this;
+    }
+
+template<typename T, typename D>
+    Vector3D<T> operator+(Vector3D<T> a, Vector3D<D> b){
+        return a+=b;
+    }
+
+template<typename T, typename D>
+    Vector3D<T> operator-(Vector3D<T> a, Vector3D<D> b){
+        return a-=b;
+    }
+
+template<typename T>
+    Vector3D<T> operator*(Vector3D<T> a, double b){
+        return a*=b;
+    }
+
+template<typename T>
+    Vector3D<T> operator*(double a, Vector3D<T> b){
+        return b*=a;
+    }
+
+template<typename T>
+    inline T Vector3D<T>::modulus() const{
+        return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    }
+
+template<typename T>
+template<typename D>
+        inline T Vector3D<T>::dot(Vector3D<D> a) const{
+            return a.x * x + a.y * y + a.z * z;
+        }
+
+template<typename T>
+    Vector3D<T>& Vector3D<T>::colorFit(){
+            if(this->x > 255){
+                this->x = 255;
+            }else if(this->x < 0){
+                this->x = 0;
+            }
+            if(this->y > 255){
+                this->y = 255;
+            }else if(this->y < 0){
+                this->y = 0;
+            }
+            if(this->z > 255){
+                this->z = 255;
+            }else if(this->z < 0){
+                this->z = 0;
+            }
+            return *this;   
+        }
+
+struct Vector2D{
+    double x, y;
+};
+    
 
 #endif
